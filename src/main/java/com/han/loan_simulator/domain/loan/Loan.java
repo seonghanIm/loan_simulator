e84a7d0d3c6a375ff35d5c27e3bd91a24b05367c
seonghanim <tjdgksdl1120@gmail.com>
2025-04-09 18:55:06 +0900

    [feat] loan 도메인 추가

---
+++ src/main/java/com/han/loan_simulator/domain/loan/Loan.java
@@ -0,0 +1,54 @@
+package com.han.loan_simulator.domain.loan;
+
+import com.han.loan_simulator.domain.loanProduct.LoanProduct;
+import com.han.loan_simulator.domain.loanProduct.RepaymentMethod;
+import com.han.loan_simulator.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="loan")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="loan_product_id", nullable = false)
+    private LoanProduct loanProduct;
+
+    // 대출 원금
+    private BigDecimal principal;
+
+    // 대출 이율
+    private BigDecimal interestRate;
+
+    // 대출 기간 (월 단위)
+    private int termMonths;
+
+    @Enumerated(EnumType.STRING)
+    private RepaymentMethod repaymentMethod;
+
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate(){
+        this.createAt = LocalDateTime.now();
+        
+    }
+
+}
